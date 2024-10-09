bd8fd12393fb86668324058f99eea1cbc6972923
Moisés Huaringa <moiseseduardo.hp@gmail.com>
2024-10-09 11:58:01 -0500

    feat: setup for releases

--- components/ui/signature.tsx
+++ components/ui/signature.tsx
@@ -1,16 +1,21 @@
 import { Text } from '@/components/ui/text';
+import { View } from 'react-native';
+import Constants from 'expo-constants';
 
 export const Signature = () => {
   return (
-    <Text>
-      {new Date().getFullYear()} &copy; Creado por {''}
-      {/* <a
-        href="https://x.com/moiseseduardohp"
-        target="_blank"
-        className="font-bold"
-      > */}
-      moiseshp
-      {/* </a> */}
-    </Text>
+    <View>
+      <Text>
+        {new Date().getFullYear()} &copy; Creado por {''}
+        {/* <a
+          href="https://x.com/moiseseduardohp"
+          target="_blank"
+          className="font-bold"
+        > */}
+        moiseshp
+        {/* </a> */}
+      </Text>
+      <Text className="text-center">v{Constants.expoConfig?.version}</Text>
+    </View>
   );
 };
